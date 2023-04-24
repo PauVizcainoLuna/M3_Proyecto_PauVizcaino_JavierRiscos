28ff0742a0dbc9dbec285b9a063cb3a5ea5b916b
viZkaHD <pauvizcainoluna@gmail.com>
2023-04-24 09:38:14 +0200

    2.0

--- src/main/java/com/mycompany/m3proyecto_javierriscos_pauvizcaino/BDConexion.java
+++ src/main/java/com/mycompany/m3proyecto_javierriscos_pauvizcaino/BDConexion.java
@@ -4,7 +4,7 @@ package com.mycompany.m3proyecto_javierriscos_pauvizcaino;
  *
  * @author PauVizcaino
  */
-
+import java.sql.*;
 public class BDConexion {
     
 }
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.m3proyecto_javierriscos_pauvizcaino {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.m3proyecto_javierriscos_pauvizcaino to javafx.fxml;
-    exports com.mycompany.m3proyecto_javierriscos_pauvizcaino;
-}
